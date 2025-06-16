9129d7ceef1e5163c3b959877f8c0978bcd76043
Santimoy Rana <santimoyrana044@gmail.com>
2025-06-16 11:09:48 +0530

    Update App.py

--- App.py
+++ App.py
@@ -11,14 +11,6 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Add security headers
-@app.after_request
-def add_security_headers(response):
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
-    response.headers['X-XSS-Protection'] = '1; mode=block'
-    return response
-
 
 # Register Blueprint
 app.register_blueprint(export_bp)
